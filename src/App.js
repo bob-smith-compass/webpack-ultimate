f0a86a8653a3cef433debd5457791de7aebc46c8
David <david.shams@gmail.com>
2019-07-23 00:22:10 -0500

    import Remarkable from 'remarkable'; MarkdownEditor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Chosen from './components/chosen/Chosen';
+import MarkdownEditor from './components/mark-down/Remarkable';
 
 export default function App() {
     return (
         <div>
             React with Webpack from scratch!
+            <Chosen />
+            <MarkdownEditor />
         </div>
     )
 }
